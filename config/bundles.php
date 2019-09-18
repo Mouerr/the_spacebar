c5f9acd66c65558cfd87dbba35c49f35c501f5c3
mouad.err <mouad@gmpanel.net>
2019-09-18 19:00:46 +0100

    + ApiTokenAuthenticator can now authenticate users by token or by email/password + Add cors bundle

--- config/bundles.php
+++ config/bundles.php
@@ -22,4 +22,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- src/Security/ApiTokenAuthenticator.php
+++ src/Security/ApiTokenAuthenticator.php
@@ -3,9 +3,11 @@
 namespace App\Security;
 
 use App\Repository\ApiTokenRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,29 +17,56 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 {
     private $apiTokenRepo;
+    private $userRepo;
+    private $passwordEncoder;
 
-    public function __construct(ApiTokenRepository $apiTokenRepo)
+    public function __construct(ApiTokenRepository $apiTokenRepo, UserRepository $userRepo, UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->apiTokenRepo = $apiTokenRepo;
+        $this->userRepo = $userRepo;
+        $this->passwordEncoder = $passwordEncoder;
     }
 
     public function supports(Request $request)
     {
-        // look for header "Authorization: Bearer <token>"
+        // look for header "Authorization: Bearer <token>" or login authentication
         return $request->headers->has('Authorization')
-            && 0 === strpos($request->headers->get('Authorization'), 'Bearer ');
+            && 0 === strpos($request->headers->get('Authorization'), 'Bearer ')
+            || strpos($request->headers->get('referer'), '/login') !== false && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
     {
         $authorizationHeader = $request->headers->get('Authorization');
+        if (!$authorizationHeader) {
+            return json_decode($request->getContent(), true); // return login credentials
+        }
 
-        // skip beyond "Bearer "
+        // skip beyond "Bearer and return token"
         return substr($authorizationHeader, 7);
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
+        if (is_array($credentials)) {
+            $user = $this->userRepo->findOneBy([
+                'email' => $credentials['email']
+            ]);
+            if (!$user instanceof UserInterface) {
+                throw new CustomUserMessageAuthenticationException(
+                    'Invalid User Mail'
+                );
+            }
+
+            $pass = $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+            if (!$pass){
+                throw new CustomUserMessageAuthenticationException(
+                    'Invalid User Password'
+                );
+            }
+            return $user;
+        }
+
         $token = $this->apiTokenRepo->findOneBy([
             'token' => $credentials
         ]);
